71e011c2997299338b0ab53a72eeb5c8605305b8
tscotce <tscotce@gmail.com>
2019-08-08 21:28:37 -0400

    progress commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,7 +18,9 @@ class PostsController < ApplicationController
       redirect to '/login' #redirecting if they aren't
     else
       post = Post.find(params[:id])
-      "An edit form #{current_user.id} is editing #{post.id}" #rendering form if they are
+      if post.user_id == current_user.id
+        "An edit form #{current_user.id} is editing #{post.id}" #rendering form if they are
+      else redirect to :'/posts'
     end
   end
 
